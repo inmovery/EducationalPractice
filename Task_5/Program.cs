a7e66c0ff643321926669c045d1b485e766bfee7
Roma Piskunov <roman_piskunov@bk.ru>
2019-04-05 21:33:17 +0500

    solve of five task

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -2,26 +2,36 @@
 
 namespace Task_5 {
     public class Program {
+        
         private static void Main(string[] args) {
             Console.Write("Порядок матрицы = ");
             int n = InputInteger();
-            double[][] matr = new double[n][];
 
-            matr = FillingMatrix(n, 1);
+            double[,] matr = new double[n,n];
 
-            PrintMatrix(matr, n);
+            FillingMatrix(ref matr, n);//сборка матрицы
 
-            Console.WriteLine(SearchMaxElement(matr, n));
+            PrintMatrix(matr, n);//вывод матрицы
+
+            Console.WriteLine(SearchMaxElement(matr, n));//вывод максимального элемента в матрице
+
+            Console.ReadKey();
 
         }
 
-        private static void PrintMatrix(double[][] matr, int n) {
+        private static void PrintMatrix(double[,] matr, int n) {
+            string pre = " ";
+            for (int i = 0; i < n; i++) {
+                pre += "————————";
+            }
+            Console.WriteLine(pre+"—");
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    Console.Write("{0,5}",matr[i][j]);
+                    Console.Write(String.Format("{0,2}{1,6:0.00}", "|", matr[i, j]));
                 }
-                Console.WriteLine();
+                Console.WriteLine(String.Format("{0,2}", "|"));
             }
+            Console.WriteLine(pre + "—");
         }
 
         
@@ -31,26 +41,22 @@ namespace Task_5 {
         /// <param name="matr">Исходная матрица</param>
         /// <param name="n">Количество строк/столбцов</param>
         /// <param name="type">Тип ввода</param>
-        private static double[][] FillingMatrix(int n, int type = 0) {
+        private static void FillingMatrix(ref double[,] matr, int n, int type = 0) {
             Random rnd = new Random();
-            double[][] fullMatrix = new double[n][];
             if (type == 0) { // рандом
                 for (int i = 0; i < n; i++) {
                     for (int j = 0; j < n; j++) {
-                        
-                        fullMatrix[i][j] = Math.Round(rnd.Next(1, 100) + rnd.NextDouble(), 2);
+                        matr[i,j] = Math.Round(rnd.Next(10, 100) + rnd.NextDouble(), 2);
                     }
                 }
             } else { // ручками
                 for (int i = 0; i < n; i++) {
                     for (int j = 0; j < n; j++) {
-                        Console.Write("Введите a[{0}][{1}] = ",i+1,j+1);
-                        fullMatrix[i][j] = InputDouble();
+                        Console.Write("Введите ({0},{1}) = ",i+1,j+1);
+                        matr[i, j] = InputDouble();
                     }
                 }
             }
-
-            return fullMatrix;
         }
 
         /// <summary>
@@ -59,20 +65,20 @@ namespace Task_5 {
         /// <param name="matr">Исходная матрица</param>
         /// <param name="n">Количество строк/столбцов</param>
         /// <returns></returns>
-        private static string SearchMaxElement(double[][] matr, int n) {
+        private static string SearchMaxElement(double[,] matr, int n) {
             double max = 0;
             int k = 0;//номер строки
             int p = 0;//номер столбца
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (((j >= i) && (n - j + 1 >= i)) || ((i >= j) && (n - j + 1 <= i)) && (matr[i][j] > max)){
-                        max = matr[i][j];
+                    if (((j >= i) && (n - j + 1 >= i)) || ((i >= j) && (n - j + 1 <= i)) && (matr[i, j] > max)){
+                        max = matr[i, j];
                         k = i;
                         p = j;
                     }
                 }
             }
-            return "Максимальный элемент: [" + k + "][" + p + "] = " + max;
+            return "Максимальный элемент: (" + (k+1) + "," + (p+1) + ") = " + max;
         }
 
         /// <summary>
