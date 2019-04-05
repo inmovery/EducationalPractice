43b83d34fc1c3fb70cc867df83587cc1bcdba9a4
Roma Piskunov <roman_piskunov@bk.ru>
2019-04-05 21:37:07 +0500

    solution of the fifth task

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 
-namespace Task_5
-{
-    public class Program
-    {
-
-        private static void Main(string[] args)
-        {
+namespace Task_5 {
+    public class Program {
+        
+        private static void Main(string[] args) {
             Console.Write("Порядок матрицы = ");
             int n = InputInteger();
 
-            double[,] matr = new double[n, n];
+            double[,] matr = new double[n,n];
 
             FillingMatrix(ref matr, n);//сборка матрицы
 
@@ -22,18 +19,14 @@ namespace Task_5
 
         }
 
-        private static void PrintMatrix(double[,] matr, int n)
-        {
+        private static void PrintMatrix(double[,] matr, int n) {
             string pre = " ";
-            for (int i = 0; i < n; i++)
-            {
+            for (int i = 0; i < n; i++) {
                 pre += "————————";
             }
-            Console.WriteLine(pre + "—");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
+            Console.WriteLine(pre+"—");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     Console.Write(String.Format("{0,2}{1,6:0.00}", "|", matr[i, j]));
                 }
                 Console.WriteLine(String.Format("{0,2}", "|"));
@@ -41,33 +34,25 @@ namespace Task_5
             Console.WriteLine(pre + "—");
         }
 
-
+        
         /// <summary>
         /// Заполнение матрицы
         /// </summary>
         /// <param name="matr">Исходная матрица</param>
         /// <param name="n">Количество строк/столбцов</param>
         /// <param name="type">Тип ввода</param>
-        private static void FillingMatrix(ref double[,] matr, int n, int type = 0)
-        {
+        private static void FillingMatrix(ref double[,] matr, int n, int type = 0) {
             Random rnd = new Random();
-            if (type == 0)
-            { // рандом
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < n; j++)
-                    {
-                        matr[i, j] = Math.Round(rnd.Next(10, 100) + rnd.NextDouble(), 2);
+            if (type == 0) { // рандом
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        matr[i,j] = Math.Round(rnd.Next(10, 100) + rnd.NextDouble(), 2);
                     }
                 }
-            }
-            else
-            { // ручками
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < n; j++)
-                    {
-                        Console.Write("Введите ({0},{1}) = ", i + 1, j + 1);
+            } else { // ручками
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        Console.Write("Введите ({0},{1}) = ",i+1,j+1);
                         matr[i, j] = InputDouble();
                     }
                 }
@@ -80,40 +65,33 @@ namespace Task_5
         /// <param name="matr">Исходная матрица</param>
         /// <param name="n">Количество строк/столбцов</param>
         /// <returns></returns>
-        private static string SearchMaxElement(double[,] matr, int n)
-        {
+        private static string SearchMaxElement(double[,] matr, int n) {
             double max = 0;
             int k = 0;//номер строки
             int p = 0;//номер столбца
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (((j >= i) && (n - j + 1 >= i)) || ((i >= j) && (n - j + 1 <= i)) && (matr[i, j] > max))
-                    {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (((j >= i) && (n - j + 1 >= i)) || ((i >= j) && (n - j + 1 <= i)) && (matr[i, j] > max)){
                         max = matr[i, j];
                         k = i;
                         p = j;
                     }
                 }
             }
-            return "Максимальный элемент: (" + (k + 1) + "," + (p + 1) + ") = " + max;
+            return "Максимальный элемент: (" + (k+1) + "," + (p+1) + ") = " + max;
         }
 
         /// <summary>
         /// Ввод натуральных, целых чисел
         /// </summary>
         /// <returns></returns>
-        private static int InputInteger()
-        {
+        private static int InputInteger() {
             bool ok = true;
             int result;
-            do
-            {
+            do {
                 string text = Console.ReadLine();
                 ok = int.TryParse(text, out result);
-                if (!ok)
-                {
+                if (!ok) {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("Ошибка. Неверный ввод!");
                     Console.Write("Введите повторно : ");
@@ -127,16 +105,13 @@ namespace Task_5
         /// Ввод действительных чисел
         /// </summary>
         /// <returns></returns>
-        private static double InputDouble()
-        {
+        private static double InputDouble() {
             bool ok = true;
             double result;
-            do
-            {
+            do {
                 string text = Console.ReadLine();
                 ok = double.TryParse(text, out result);
-                if (!ok)
-                {
+                if (!ok) {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("Ошибка. Неверный ввод!");
                     Console.Write("Введите повторно : ");
@@ -146,5 +121,5 @@ namespace Task_5
             return result;
         }
 
-    }
+   }
 }
